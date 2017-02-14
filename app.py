b950e942dd19c09e9fe6b9d085d409e06d2d6337
James Strassburg <jstrassburg@gmail.com>
2017-02-14 15:48:45 -0600

    host = 0.0.0.0

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     return json.dumps(os.environ, default=lambda o: o.__dict__, sort_keys=True, indent=2)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
